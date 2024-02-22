a95d5af25fd69f87477c2373d9a3a7976509da9b
hyeonLewis <lewis.kim@klaytn.foundation>
2024-02-22 14:13:20 +0900

    Update contracts

--- src/constants/Address.ts
+++ src/constants/Address.ts
@@ -8,11 +8,12 @@ interface Addresses {
     oidcRecoveryFactoryV02Addr?: string;
     zkVerifierV02Addr?: string;
     manualJwksProviderAddr?: string;
+    oraklJwksProviderAddr?: string;
     googleGuardianV02Addr?: string;
     kakaoGuardianV02Addr?: string;
+    twitchGuardianV02Addr?: string;
     appleGuardianV02Addr?: string;
     lineGuardianV02Addr?: string;
-    twitchGuardianV02Addr?: string;
     counterAddr?: string;
 }
 
@@ -21,24 +22,26 @@ const CYPRESS: Addresses = {
     oidcRecoveryFactoryV02Addr: undefined,
     zkVerifierV02Addr: undefined,
     manualJwksProviderAddr: undefined,
+    oraklJwksProviderAddr: undefined,
     googleGuardianV02Addr: undefined,
     kakaoGuardianV02Addr: undefined,
+    twitchGuardianV02Addr: undefined,
     appleGuardianV02Addr: undefined,
     lineGuardianV02Addr: undefined,
-    twitchGuardianV02Addr: undefined,
     counterAddr: undefined,
 };
 
 const BAOBAB: Addresses = {
     entryPointAddr: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
-    oidcRecoveryFactoryV02Addr: "0xd717B50719324eb321E884712a5cDc33d868aF88",
+    oidcRecoveryFactoryV02Addr: "0x5A454a213BeD09052c6b84a4344a8eD3A69D559c",
     zkVerifierV02Addr: "0xC7B94E3827FD4D2c638EEae2e9219Da063b5BB55",
     manualJwksProviderAddr: "0xF871E80Ac5F679f9137Db4091841F0657dFD2B04",
-    googleGuardianV02Addr: "0xcB843F8D70011095E19BE0Bb081A59876b6E69c3",
-    kakaoGuardianV02Addr: "0x635179B11a5458A4840Dfc6fD0c0a1601cf3CF35",
+    oraklJwksProviderAddr: "0x993BcF72A45c834A12a04d095B7961472325B6A2",
+    googleGuardianV02Addr: "0x18B64ff66D993f8875AdA004dD3e11Da61576B35",
+    kakaoGuardianV02Addr: "0x7D3f210ae1E50E40902515D7CDFb7d7Af9dF8323",
+    twitchGuardianV02Addr: "0xaE99480B3FaaB2A79083513b4d8EaD3c04cC6353",
     appleGuardianV02Addr: undefined,
     lineGuardianV02Addr: undefined,
-    twitchGuardianV02Addr: "0xe5e77a14CF722B4C0C6d5d140ef9B0b17105Cf60",
     counterAddr: "0x3F2201Db69c7bD8427FD816ca4d38CC17B448d90",
 };
 
